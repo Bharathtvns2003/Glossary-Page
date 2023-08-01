46ca6db57c695c805849aa39a1f38112f66bf504
bharathtvns2003 <venkata.naga@dezerv.in>
2023-08-01 17:21:54 +0530

    box fit

--- src/Body.js
+++ src/Body.js
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react'
-import './Body.css'
+import React, { useEffect, useState } from 'react';
+import './Body.css';
 
 function Body() {
   const [activeSection, setActiveSection] = useState('A');
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [isMobileDevice, setIsMobileDevice] = useState(false);
 
   useEffect(() => {
     const sectionElement = document.getElementById(activeSection);
@@ -11,64 +13,69 @@ function Body() {
     }
   }, [activeSection]);
 
-  useEffect(() => {
-    const isMobileDevice = window.innerWidth <= 768;
-    const sidebar = document.querySelector('.Sidebar');
+  const handleScroll = () => {
     const headingA = document.querySelector('.Body_SubHeading');
+    const headingRect = headingA.getBoundingClientRect();
+    const isPassedHeadingA = headingRect.bottom <= 0;
 
-    const handleScroll = () => {
-      const headingRect = headingA.getBoundingClientRect();
-      const isPassedHeadingA = headingRect.bottom <= 0;
-
-      if (isPassedHeadingA && !isMobileDevice ) {
-        sidebar.style.display = 'flex'; // Show the sidebar when not passing through heading A
-      } else {
-        sidebar.style.display = 'none'; // Hide the sidebar when passing through heading A
+    if (isPassedHeadingA) {
+      if (isMobileDevice) {
+        setShowSidebar(true);
       }
-      
-    };
+    } else {
+      setShowSidebar(false);
+    }
+  };
 
-    // Attach the scroll event listener to the window
+  const handleResize = () => {
+    setIsMobileDevice(window.innerWidth <= 768);
+  };
+
+  useEffect(() => {
     window.addEventListener('scroll', handleScroll);
+    window.addEventListener('resize', handleResize);
 
-    // Cleanup: Remove the event listener when the component unmounts
+    // Call handleScroll and handleResize once to set initial state
+    handleScroll();
+    handleResize();
+
+    // Cleanup event listeners on component unmount
     return () => {
       window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', handleResize);
     };
-  }, []);
+  }, [isMobileDevice, activeSection]);
+
 
   return (
     <div className='Body'>
-      <div className='Sidebar'>
+      <div className={`Sidebar ${showSidebar ? 'show' : 'hide'}`}>
         <button onClick={() => setActiveSection('A')}>A</button>
-        <button onClick={() => setActiveSection('B')}>.</button>
-        <button onClick={() => setActiveSection('C')}>.</button>
+        <button onClick={() => setActiveSection('B')}>B</button>
+        <button onClick={() => setActiveSection('C')}>C</button>
         <button onClick={() => setActiveSection('D')}>D</button>
-        <button onClick={() => setActiveSection('E')}>.</button>
-        <button onClick={() => setActiveSection('F')}>.</button>
+        <button onClick={() => setActiveSection('E')}>E</button>
+        <button onClick={() => setActiveSection('F')}>F</button>
         <button onClick={() => setActiveSection('G')}>G</button>
-        <button onClick={() => setActiveSection('H')}>.</button>
-        <button onClick={() => setActiveSection('I')}>.</button>
+        <button onClick={() => setActiveSection('H')}>H</button>
+        <button onClick={() => setActiveSection('I')}>I</button>
         <button onClick={() => setActiveSection('J')}>J</button>
-        <button onClick={() => setActiveSection('K')}>.</button>
-        <button onClick={() => setActiveSection('L')}>.</button>
+        <button onClick={() => setActiveSection('K')}>K</button>
+        <button onClick={() => setActiveSection('L')}>L</button>
         <button onClick={() => setActiveSection('M')}>M</button>
-        <button onClick={() => setActiveSection('N')}>.</button>
-        <button onClick={() => setActiveSection('O')}>.</button>
+        <button onClick={() => setActiveSection('N')}>N</button>
+        <button onClick={() => setActiveSection('O')}>O</button>
         <button onClick={() => setActiveSection('P')}>P</button>
-        <button onClick={() => setActiveSection('Q')}>.</button>
-        <button onClick={() => setActiveSection('R')}>.</button>
+        <button onClick={() => setActiveSection('Q')}>Q</button>
+        <button onClick={() => setActiveSection('R')}>R</button>
         <button onClick={() => setActiveSection('S')}>S</button>
-        <button onClick={() => setActiveSection('T')}>.</button>
-        <button onClick={() => setActiveSection('U')}>.</button>
+        <button onClick={() => setActiveSection('T')}>T</button>
+        <button onClick={() => setActiveSection('U')}>U</button>
         <button onClick={() => setActiveSection('V')}>V</button>
-        <button onClick={() => setActiveSection('W')}>.</button>
-        <button onClick={() => setActiveSection('X')}>.</button>
+        <button onClick={() => setActiveSection('W')}>W</button>
+        <button onClick={() => setActiveSection('X')}>X</button>
         <button onClick={() => setActiveSection('Y')}>Y</button>
         <button onClick={() => setActiveSection('Z')}>Z</button>
-
-
-        {/* Add the rest of the alphabet buttons here */}
       </div>
       <div className='A_Z_Box'>
         <button onClick={() => setActiveSection('A')}>A</button>
